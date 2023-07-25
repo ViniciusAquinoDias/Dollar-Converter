fa3287eae7db3aa7096708e7db15164b8c194f5c
Vinicius Aquino Dias <vdeaquinodias@gmail.com>
2023-07-25 13:21:05 -0300

    1.0

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿double dolarToReal = 4.73;
+
+Console.Write("conversion Dólar to Real\n\n");
+
+Console.Write("How many dólar banknotes do you have? ");
+double banknotes = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine($"So you have R$:{dolarToReal * banknotes} in conversion");
